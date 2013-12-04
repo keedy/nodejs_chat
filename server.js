e8ece84dd8f1e14a1c167c70793352456788a874
keedy <keedy@keedy.megivps.pl>
2013-12-04 22:05:10 +0100

    Mobed app.js to server.js

--- server.js
+++ server.js
@@ -1,8 +1,69 @@
-var http = require('http');
-var ip = process.env.APP_IP || '0.0.0.0';
-var port = parseInt(process.env.APP_PORT || '8000');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello, node.js World\n');
-}).listen(port, ip);
-console.log('Server running at http://' + ip + ':' + port + '/');
+var express = require('express'),
+	app = express(),
+	server = require('http').createServer(app),
+	redis = require('redis'),
+	store = redis.createClient(),
+	jade = require('jade'),
+	io = require('socket.io').listen(server);
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.set("view options", { layout: false });
+app.configure(function() {
+   app.use(express.static(__dirname + '/public'));
+});
+
+app.get('/', function(req, res) {
+	res.render('index.jade');
+});
+
+var config = require('./config');
+
+server.listen(config.port);
+
+function users(redis, channel) {
+	redis.smembers(config.users + '_' + channel, function(err, members) {
+		var users = [];
+
+		for(i = 0; i < members.length; ++i) {
+			var id = members[i];
+			redis.hgetall(userSession + '_' + channe; + '_' + channell, function(err, userData) {
+				users.push({
+					'nickname': userData.nickname
+				})
+			})
+		}
+	})
+}
+
+io.sockets.on('connection', function (socket) {
+	socket.on('setNickname', function (nickname) {
+   		socket.set('nickname', nickname);
+	});
+
+	socket.on('join', function(room) {
+		socket.set('room', room);
+		socket.get('nickname', function(err, nickname) {
+   			store.sadd('users_' + room, nickname);
+   		})
+	});
+
+	socket.on('updateUsers', function(users) {
+   		var users = [];
+		socket.get('room', function(err, room) {
+			store.smembers('users_' + room, function(err, members) {
+
+   			});
+		});
+	});
+
+	socket.on('message', function (message) {
+		socket.get('nickname', function(error, nickname) {
+			socket.get('room', function(error, room) {
+				var data = {'message' : message, 'nickname' : nickname, 'room': room};
+				socket.broadcast.emit('message', data);
+				console.log('user ' + nickname + ' send: ' + message + ' at room: ' + room);
+			});
+	   });
+	});
+});
