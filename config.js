65ef1c51e075e4f4937e89dc9a86af7bc518cf11
Keedy <piotr.biernat@gmail.com>
2013-12-07 23:44:33 +0100

    cleaned config.js

--- config.js
+++ config.js
@@ -1,8 +1,28 @@
-module.exports = {
-	ip: process.env.APP_IP || '0.0.0.0',
-	port: parseInt(process.env.APP_PORT || '8000'),
-	messageId: 'chat-message',
-	history: 'chat-history',
-	users: 'chat-users',
-	session: 'chat-session',
+exports.server = {
+	host: process.env.APP_IP || '0.0.0.0',
+	port: parseInt(process.env.APP_PORT || '8000')
+};
+
+exports.redis = {
+	host: process.env.REDIS_HOST || 'localhost',
+	port: parseInt(process.env.REDIS_PORT || '6379'),
+	password: process.env.REDIS_PASSWORD || undefined,
+	log_level: parseInt(process.env.REDIS_LOG_LEVEL || '3')
+};
+
+exports.chat = {
+	user_join_message: 'User %nickname$ joined',
+	user_leave_message: 'User %nickname% has left',
+
+	writing_icon_enabled: false,
+
+	display_emoticons: true,
+	emoticons_filename: 'default.png',
+
+	anti_flood_protection: true,
+	anti_spam_protection: true,
+
+	admin_list: {
+		'keedy', 'keedy2'
+	}
 }
