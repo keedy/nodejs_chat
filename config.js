f616bbf002b2dd9e6669a17976fc9382467538c7
Keedy <piotr.biernat@gmail.com>
2013-12-12 21:25:53 +0100

    Refactored

--- config.js
+++ config.js
@@ -15,31 +15,31 @@ exports.chat = {
 		main_room_name: 'chat'
 	},
 	events: {
-		user_join: { // displayed when user join chat
+		user_join: {
 			enabled: true,
 			message: 'User %nickname% joined'
 		},
-		user_leave: { // displayed when user leave chat
+		user_leave: {
 			enabled: true,
 			message: 'User %nickname% leave'
 		},
-		topic_changed: { // displayed when topic was changed
+		topic_changed: {
 			enabled: true,
 			message: 'Topic was changed from %old_topic% to %new_topic%'
 		},
-		new_message_public_message: {
+		new_public_message: {
 			play_sound: true,
-			audio_file: '/beep.mp3',
+			sound_file: '/beep.mp3',
 			show_in_title: true
 		},
 		new_private_message: {
 			play_sound: true,
-			audio_file: '/beep.mp3',
+			sound_file: '/beep.mp3',
 			show_in_title: true
 		}
 	},
 	behaviors: {
-		user_writing_message: { // display pencil icon or text notification when user writes message(only in private chat)
+		user_writing_message: {
 			display_icon: true,
 			icon_file: '/writing_icon.gif',
 			display_text: false,
@@ -52,18 +52,18 @@ exports.chat = {
 	},
 	security: {
 		flood: {
-			prevent: true, // true or false
-			max_messages_per_second: 3, // time in seconds
-			first_warning: 'kick', // kick, temp_ban or perm_ban
-			second_warning: 'temp_ban', // kick, temp_ban or perm_ban
-			third_warning: 'perm_ban', // kick, temp_ban or perm_ban
+			prevent: true,
+			max_messages_per_second: 3,
+			first_warning: 'kick',
+			second_warning: 'temp_ban',
+			third_warning: 'perm_ban',
 		},
 		spam: {
 			block_links: true,
 			block_emails: true,
-			first_warning: 'kick', // kick, temp_ban or perm_ban
-			second_warning: 'temp_ban', // kick, temp_ban or perm_ban
-			third_warning: 'perm_ban', // kick, temp_ban or perm_ban
+			first_warning: 'kick',
+			second_warning: 'temp_ban',
+			third_warning: 'perm_ban',
 		},
 		censorship: {
 			enabled: true,
@@ -82,9 +82,9 @@ exports.chat = {
 				'^(pizd).*$',
 				'^(ciul).*$'
 			],
-			first_warning: 'kick', // kick, temp_ban or perm_ban
-			second_warning: 'temp_ban', // kick, temp_ban or perm_ban
-			third_warning: 'perm_ban', // kick, temp_ban or perm_ban
+			first_warning: 'kick',
+			second_warning: 'temp_ban',
+			third_warning: 'perm_ban',
 		},
 		admins: ['keedy', 'keedy2']
 	}
